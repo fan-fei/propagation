3d896d2db8df61a321024479dc2a4ddb96ff52e4
fanfei <chris.aimee.2012@gmail.com>
2018-03-27 19:15:20 +0800

    update

--- propagation/src/main/java/propagation/entity/User.java
+++ propagation/src/main/java/propagation/entity/User.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
@@ -38,6 +38,5 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
 
 }
