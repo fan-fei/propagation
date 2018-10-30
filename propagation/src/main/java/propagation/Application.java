a3b06a5448dd69baf0abb70016c7d6a77f320091
樊飞 <fanfei@ctv.org.cn>
2018-10-30 13:01:58 +0800

    update

--- propagation/src/main/java/propagation/Application.java
+++ propagation/src/main/java/propagation/Application.java
@@ -10,4 +10,15 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+//    |ANNOTATION-----------------|TRANSACTION EXISTS-------------------|NONE EXISTS------------------|
+//    |---------------------------|-------------------------------------|-----------------------------|
+//    |PROPAGATION_REQUIRED-------|SUPPORT------------------------------|CREATE-----------------------|
+//    |PROPAGATION_SUPPORTS-------|SUPPORT------------------------------|EXECUTE NON-TRANSACTIONALLY--|
+//    |PROPAGATION_MANDATORY------|SUPPORT------------------------------|THROW AN EXCEPTION-----------|
+//    |PROPAGATION_REQUIRES_NEW---|SUSPEND------------------------------|CREATE-----------------------|
+//    |PROPAGATION_NOT_SUPPORTED--|SUSPEND------------------------------|EXECUTE NON-TRANSACTIONALLY--|
+//    |PROPAGATION_NEVER----------|THROW AN EXCEPTION-------------------|EXECUTE NON-TRANSACTIONALLY--|
+//    |PROPAGATION_NESTED---------|EXECUTE WITHIN A NESTED TRANSACTION--|CREATE-----------------------|
+//    |---------------------------|-------------------------------------|-----------------------------|
+
 }
