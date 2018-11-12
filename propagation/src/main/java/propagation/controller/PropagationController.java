5bd9251eefc4659e1a2e2f7890afccf0caa229f8
樊飞 <fanfei@ctv.org.cn>
2018-11-12 13:23:28 +0800

    update

--- propagation/src/main/java/propagation/controller/PropagationController.java
+++ propagation/src/main/java/propagation/controller/PropagationController.java
@@ -2,7 +2,7 @@ package propagation.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,7 +17,7 @@ public class PropagationController {
     @Autowired
     private PropagationService1 propagationService1;
 
-    @GetMapping(path = "/users")
+    @PostMapping(path = "/users")
     public @ResponseBody Boolean addUser(@RequestBody User user) {
         return propagationService1.addUser(user);
     }
