19701a06c91150cba0de475a80673e83da21c50a
樊飞 <fanfei@ctv.org.cn>
2019-03-18 14:17:00 +0800

    use mysql8.0.12

--- propagation/src/main/java/propagation/entity/Department.java
+++ propagation/src/main/java/propagation/entity/Department.java
@@ -1,22 +1,29 @@
 package propagation.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
 @Entity
 @Data
-public class User {
+public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer code;
 
-    private String name;
+    @Column(length = 20)
+    private String depName;
 
-    private String email;
+    @OneToMany
+    private List<Employee> employeeList = new ArrayList<>();
 
 }
--- propagation/src/main/java/propagation/entity/Employee.java
+++ propagation/src/main/java/propagation/entity/Employee.java
@@ -1,22 +1,29 @@
 package propagation.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 
 @Entity
 @Data
-public class User {
+public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer code;
 
-    private String name;
+    @Column(length = 20)
+    private String empName;
 
-    private String email;
+    @Column(length = 20)
+    private String phone;
+
+    @ManyToOne
+    private Department department;
 
 }
