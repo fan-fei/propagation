859c7a1b14ee775d765a8f53e8eee65ee76f7048
樊飞 <fanfei@ctv.org.cn>
2019-03-25 17:13:38 +0800

    BubbleSort

--- propagation/src/main/java/sort/BubbleSort.java
+++ propagation/src/main/java/sort/BubbleSort.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int arr[] = new int[] { 10, 9, 8, 7, 3, 3, 5, 4, 1, 2 };
+        int arr[] = new int[] { 10, 9, 4, 7, 3, 3, 5, 6, 1, 0 };
         sort(arr);
     }
 
     private static void sort(int arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 1; j < arr.length  - i; j++) {
-                if (arr[j-1] > arr[j]) {
-                    int temp = arr[j-1];
-                    arr[j-1] = arr[j];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    int temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
                     arr[j] = temp;
                 }
             }
