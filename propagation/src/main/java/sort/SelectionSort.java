5f581b178844517cd96b1ee1dd264c180682858b
樊飞 <fanfei@ctv.org.cn>
2019-03-25 18:08:19 +0800

    SelectionSort

---
+++ propagation/src/main/java/sort/SelectionSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        sort(arr);
+    }
+
+    private static void sort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(arr.length);
+
+    }
+}
