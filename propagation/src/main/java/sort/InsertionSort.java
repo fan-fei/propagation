6256c4ac95f89c15f831d4981111938d45cf34a1
樊飞 <fanfei@ctv.org.cn>
2019-03-26 15:46:59 +0800

    InsertionSort

--- propagation/src/main/java/sort/InsertionSort.java
+++ propagation/src/main/java/sort/InsertionSort.java
@@ -2,7 +2,7 @@ package sort;
 
 import java.util.Arrays;
 
-public class BubbleSort {
+public class InsertionSort {
     public static void main(String[] args) {
         int arr[] = new int[] { 10, 9, 4, 7, 3, 3, 5, 6, 1, 0 };
         sort(arr);
@@ -10,14 +10,16 @@ public class BubbleSort {
     }
 
     private static void sort(int arr[]) {
-        for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = 1; j <= i; j++) {
-                if (arr[j - 1] > arr[j]) {
-                    int temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
+
     }
 }
--- propagation/src/main/java/sort/SelectionSort.java
+++ propagation/src/main/java/sort/SelectionSort.java
@@ -6,6 +6,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         int arr[] = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
         sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     private static void sort(int arr[]) {
@@ -20,9 +21,5 @@ public class SelectionSort {
             arr[min] = arr[i];
             arr[i] = temp;
         }
-
-        System.out.println(Arrays.toString(arr));
-        System.out.println(arr.length);
-
     }
 }
