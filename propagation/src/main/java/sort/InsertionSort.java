6a76594002bf9b142f1a186e25ad6e229a430b91
樊飞 <fanfei@ctv.org.cn>
2019-03-26 17:34:20 +0800

    ShellSort

--- propagation/src/main/java/sort/InsertionSort.java
+++ propagation/src/main/java/sort/InsertionSort.java
@@ -13,10 +13,12 @@ public class InsertionSort {
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = i - 1; j >= 0; j--) {
-                if (arr[j] > arr[j+1]) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                } else {
+                    break;
                 }
             }
         }
---
+++ propagation/src/main/java/sort/ShellSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.util.Arrays;
+
+public class ShellSort {
+    public static void main(String[] args) {
+        int arr[] = new int[] { 10, 9, 4, 7, 3, 3, 5, 6, 1, 0 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void sort(int arr[]) {
+
+        for (int k = arr.length / 2; k > 0; k /= 2) {
+            for (int i = 0; i < arr.length; i += k) {
+                for (int j = i - k; j >= 0; j -= k) {
+                    if (arr[j] > arr[j + k]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + k];
+                        arr[j + k] = temp;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
