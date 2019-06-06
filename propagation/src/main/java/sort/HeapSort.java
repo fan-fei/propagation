b909521f54f3c3d4de9a0cf90bd6dbcdd32ef821
樊飞 <fanfei@ctv.org.cn>
2019-06-06 17:12:05 +0800

    heapSort

---
+++ propagation/src/main/java/sort/HeapSort.java
@@ -0,0 +1,22 @@
+package sort;
+
+import java.util.Arrays;
+
+public class HeapSort {
+
+    public static void main(String[] args) {
+        int arr[] = new int[] { 10, 9, 4, 7, 3, 3, 5, 6, 1, 0 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void sort(int arr[]) {
+
+        int start = arr.length / 2;
+        for (int i = start; i >= 0; i--) {
+
+        }
+
+    }
+}
