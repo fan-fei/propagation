360d9f4e5bd2da12b3081ad96a87adceb5328145
樊飞 <fanfei@ctv.org.cn>
2019-06-06 17:48:48 +0800

    update

--- propagation/src/main/java/sort/HeapSort.java
+++ propagation/src/main/java/sort/HeapSort.java
@@ -7,14 +7,15 @@ public class HeapSort {
     public static void main(String[] args) {
         int arr[] = new int[] { 10, 9, 4, 7, 3, 3, 5, 6, 1, 0 };
         sort(arr);
+
         System.out.println(Arrays.toString(arr));
 
     }
 
     private static void sort(int arr[]) {
 
-        int start = arr.length / 2;
-        for (int i = start; i >= 0; i--) {
+        int current = arr.length / 2;
+        for (int i = current; i >= 0; i--) {
 
         }
 
