6441eeb59865a563a7fb81978479a935f872e1b3
樊飞 <fanfei@ctv.org.cn>
2019-06-13 14:50:15 +0800

    heapSort

--- propagation/src/main/java/sort/HeapSort.java
+++ propagation/src/main/java/sort/HeapSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class HeapSort {
 
     public static void main(String[] args) {
-        int arr[] = new int[] { 10, 9, 4, 7, 3, 3, 5, 6, 1, 0 };
+        int arr[] = new int[] { 10, 9, 4, 7, 5, 5, 6, 1, 0, 2 };
         sort(arr);
 
         System.out.println(Arrays.toString(arr));
@@ -13,11 +13,40 @@ public class HeapSort {
     }
 
     private static void sort(int arr[]) {
+        int lastIndex = arr.length - 1;
 
-        int current = arr.length / 2;
-        for (int i = current; i >= 0; i--) {
-
+        for (int currentIndex = (lastIndex - 1) / 2; currentIndex >= 0; currentIndex--) {
+            upDownSwap(arr, currentIndex, lastIndex);
         }
 
+        while (lastIndex > 0) {
+            int temp = arr[lastIndex];
+            arr[lastIndex] = arr[0];
+            arr[0] = temp;
+
+            lastIndex--;
+            upDownSwap(arr, 0, lastIndex);
+        }
+    }
+
+    private static void upDownSwap(int[] arr, int currentIndex, int lastIndex) {
+        int leftIndex = currentIndex * 2 + 1;
+        int rightIndex = currentIndex * 2 + 2;
+
+        if (leftIndex <= lastIndex && arr[currentIndex] < arr[leftIndex]) {
+            int temp = arr[currentIndex];
+            arr[currentIndex] = arr[leftIndex];
+            arr[leftIndex] = temp;
+
+            upDownSwap(arr, leftIndex, lastIndex);
+        }
+
+        if (rightIndex <= lastIndex && arr[currentIndex] < arr[rightIndex]) {
+            int temp = arr[currentIndex];
+            arr[currentIndex] = arr[rightIndex];
+            arr[rightIndex] = temp;
+
+            upDownSwap(arr, rightIndex, lastIndex);
+        }
     }
 }
